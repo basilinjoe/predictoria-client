38e8ede79e51f4f68fd93d12fc25a44dee405c14
Basilin Joe <basilinjoe@gmail.com>
2018-11-11 22:34:46 +0530

    Add snack bar to notify login failure

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from "@angular/router";
+import {MatSnackBar} from '@angular/material';
 
 export interface LoginResponse {
   userName: string,
@@ -19,7 +20,7 @@ export class LoginComponent implements OnInit {
     userName: new FormControl('',Validators.required),
     password:new FormControl('',Validators.required)
   });
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router, public snackBar: MatSnackBar) { }
 
   ngOnInit() {
   }
@@ -27,6 +28,10 @@ export class LoginComponent implements OnInit {
   onSubmit(){
     const resultGetUrl = `http://localhost:9091/api/login`;
     this.http.post<LoginResponse>(resultGetUrl, this.loginForm.value).subscribe(res=>{
+      if(!res) {
+        this.snackBar.open('Invalid username or password !','Try again',{duration:2000});
+        return;
+      }
       if(res.isAdmin){
         this.router.navigate(['dashboardAdmin', res.userName]);
       }
