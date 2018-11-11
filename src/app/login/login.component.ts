28271229975d21d5ed40a24d207288bcf9358ce0
Basilin Joe <basilinjoe@gmail.com>
2018-11-11 22:25:25 +0530

    Add login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from "@angular/router";
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-};
+export interface LoginResponse {
+  userName: string,
+  password: string,
+  isAdmin: boolean
+}
 
 @Component({
   selector: 'app-login',
@@ -13,15 +15,25 @@ export interface Tile {
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  tiles: Tile[] = [
-    {text: 'One', cols: 4, rows: 3, color: 'lightblue'},
-    {text: 'Two', cols: 4, rows: 3, color: 'lightblue'},
-    {text: 'Three', cols: 4, rows: 3, color: 'lightblue'}
-    
-  ];
-  constructor() { }
+  loginForm = new FormGroup({
+    userName: new FormControl('',Validators.required),
+    password:new FormControl('',Validators.required)
+  });
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    const resultGetUrl = `http://localhost:9091/api/login`;
+    this.http.post<LoginResponse>(resultGetUrl, this.loginForm.value).subscribe(res=>{
+      if(res.isAdmin){
+        this.router.navigate(['dashboardAdmin', res.userName]);
+      }
+      else{
+        this.router.navigate(['dashboard', res.userName]);
+      }
+    });
   }
 
 }
