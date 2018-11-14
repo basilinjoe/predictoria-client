8f50a64ed1d5ea807944127d48cb81e6cee931d4
Basilin Joe <basilinjoe@gmail.com>
2018-11-14 13:02:16 +0530

    Add redirect

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import { HttpClient } from '@angular/common/http';
-
+import { Router } from "@angular/router";
 import { UserResult } from './user-result';
 
 @Component({
@@ -18,7 +18,7 @@ export class DashboardComponent implements OnInit {
     userName:''
   };
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {
+  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {
     this.route.params.subscribe( params => this.userId = params['userId'] );
    }
 
@@ -31,6 +31,10 @@ export class DashboardComponent implements OnInit {
     this.http.get<UserResult>(resultGetUrl).subscribe(res=>{
       this.userInfo = res;
     });
+  }
+
+  goToPredict(){
+    this.router.navigate([`history/${this.userId}`]);
   }
 
 }
